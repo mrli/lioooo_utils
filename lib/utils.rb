50afba44aa62d6abc21a033077b530d5f362247f
mrli <lb563@foxmail.com>
2014-04-14 11:36:24 +0800

    add check method <is_dev?> witch check if is local development mac

--- lib/utils.rb
+++ lib/utils.rb
@@ -1,9 +1,16 @@
 #encoding: utf-8
+require 'socket'
+Server = UDPSocket.open { |s| s.connect('64.233.187.99', 1); s.addr.last }
 class LiooooUtils
   class << self
     def build_log_dir source, target
       cmd = "ln -sf #{source} #{target}"
       ShellExec.exec! cmd
+    end
+
+    # check if is development mac
+    def is_dev?
+      Server =~/^192.168/
     end
   end
 end
