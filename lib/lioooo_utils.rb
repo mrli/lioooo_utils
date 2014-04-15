d3ba4a938041ae3aa5ceff94d09c65a425b9cf84
mrli <lb563@foxmail.com>
2014-04-15 11:01:16 +0800

    add gen sig method

--- lib/lioooo_utils.rb
+++ lib/lioooo_utils.rb
@@ -1,5 +1,5 @@
 #encoding: utf-8
-Root=File.expand_path('../../',__FILE__)
-require "#{Root}/lib/logs/lioooo_log"
+Root=File.expand_path('../../', __FILE__)
+require "#{Root}/lib/lioooo_log"
 require "#{Root}/lib/utils"
-require "#{Root}/lib/version"
+require "#{Root}/lib/sigcheck"
--- lib/sigcheck.rb
+++ lib/sigcheck.rb
@@ -2,30 +2,36 @@
 require 'cgi'
 require 'digest/sha1'
 require 'base64'
-class SigCheck
+class LiooooUtils
+  module SigCheck
+    class << self
+      #生成URL签名
+      #@param string 	method 请求方法 "get" or "post"
+      #@param string 	url_path
+      #@param array 	params 表单参数
+      #@param string 	secret 密钥
+      def makeSig(method, url_path, params, secret)
+        mk = makeSource(method, url_path, params)
+        _secret = secret.tr('-_', '+/')
+        my_sign = Digest::HMAC.digest(mk, _secret, Digest::SHA1)
+        my_sign = Base64.encode64(my_sign).gsub("\n", "")
+        return my_sign
+      end
 
-  #生成签名
-  #@param string 	method 请求方法 "get" or "post"
-  #@param string 	url_path
-  #@param array 	params 表单参数
-  #@param string 	secret 密钥
-  def self.makeSig(method, url_path, params, secret)
-    mk = makeSource(method, url_path, params)
-    _secret = secret.tr('-_', '+/')
-    my_sign = Digest::HMAC.digest(mk, _secret, Digest::SHA1)
-    my_sign = Base64.encode64(my_sign).gsub("\n", "")
-    return my_sign
+      def makeSource(method, url_path, params)
+        _method = method.upcase
+        _url_path = CGI.escape(url_path)
+        _query_string = params.sort.collect { |v| v.join('=') }.join('&')
+        query_string = CGI.escape(_query_string)
+        _strs = "#{_method}&#{_url_path}&"
+        strs = _strs.gsub('~', '%7E')
+        _source_str = "#{strs}#{query_string}".gsub('+', '%20')
+        return _source_str
+      end
+
+    end
+
   end
-
-  def self.makeSource(method, url_path, params)
-    _method = method.upcase
-    _url_path = CGI.escape(url_path)
-    _query_string = params.sort.collect { |v| v.join('=') }.join('&')
-    query_string = CGI.escape(_query_string)
-    _strs = "#{_method}&#{_url_path}&"
-    strs = _strs.gsub('~', '%7E')
-    _source_str = "#{strs}#{query_string}".gsub('+', '%20')
-    return _source_str
-  end
-
 end
+
+#puts LiooooUtils::SigCheck.makeSig('xxx',"/sddd",{},"sdfsdfs");
